92e64fa58b1fb87f400d2bc33dfb1c66199da174
absum18 <absum1802@gmail.com>
2025-10-24 19:18:37 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 const Contact = () => {
   const phoneNumber = "9974349932";
   const address = "Samodiya Fariya, Eru Chaar Rasta, Navsari, Gujarat";
-  const mapUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.5!2d72.95!3d20.95!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjDCsDU3JzAwLjAiTiA3MsKwNTcnMDAuMCJF!5e0!3m2!1sen!2sin!4v1234567890";
+  const mapUrl = "https://maps.app.goo.gl/BdWjNXKfJaXSApPD7";
 
   const handleCall = () => {
     window.location.href = `tel:${phoneNumber}`;
